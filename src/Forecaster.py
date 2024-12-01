d36ff744a1fb1387251bb709a2a000125ea9dd09
Luca <luca24ever@gmail.com>
2024-12-01 11:49:47 +0100

    Final commit

--- src/Forecaster.py
+++ src/Forecaster.py
@@ -6,7 +6,14 @@ import warnings
 from tqdm import tqdm
 from typing import List
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, WhiteKernel, ConstantKernel, ExpSineSquared, DotProduct, Matern
+from sklearn.gaussian_process.kernels import (
+    RBF,
+    WhiteKernel,
+    ConstantKernel,
+    ExpSineSquared,
+    DotProduct,
+    Matern,
+)
 from statsmodels.tsa.vector_ar.var_model import VAR
 import shap
 
@@ -34,7 +41,9 @@ def fill_missing_values_with_gp(df):
                 kernel = WhiteKernel(0.1, PARAM_RANGES)
                 kernel += ConstantKernel(1, PARAM_RANGES) * RBF(1, PARAM_RANGES)
                 kernel += ConstantKernel(1, PARAM_RANGES) * DotProduct(1, PARAM_RANGES)
-                gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, alpha=1e-2)
+                gp = GaussianProcessRegressor(
+                    kernel=kernel, n_restarts_optimizer=10, alpha=1e-2
+                )
 
                 # Fit GP to observed data
                 gp.fit(X_observed, y_observed)
@@ -46,8 +55,9 @@ def fill_missing_values_with_gp(df):
                 df.loc[df[col].isna(), col] = y_missing_pred
     return df
 
+
 class ExternalAction:
-    def __init__(self, var:str, amount, time, duration, type: str):
+    def __init__(self, var: str, amount, time, duration, type: str):
         self.var = var
         self.amount = amount
         self.time = time
@@ -58,9 +68,8 @@ class ExternalAction:
             raise Exception()
 
     def is_applicable(self, time):
-        return time >= self.time and time <= self.time + self.duration 
+        return time >= self.time and time <= self.time + self.duration
 
-    
 
 class Forecaster:
     def __init__(self):
@@ -78,17 +87,16 @@ class Forecaster:
         kernel += ConstantKernel(1, PARAM_RANGES) * RBF(1, PARAM_RANGES)
         kernel += ConstantKernel(1, PARAM_RANGES) * DotProduct(1, PARAM_RANGES)
 
-
-
         # PARAM_RANGES = (1e-4, 1e4)
         # kernel = WhiteKernel(0.1, PARAM_RANGES)
         # kernel += ConstantKernel(1, PARAM_RANGES) * RBF(1, PARAM_RANGES)
         # kernel += ConstantKernel(1, PARAM_RANGES) * DotProduct(1, PARAM_RANGES)
 
-        self.gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9, random_state=42)
+        self.gp = GaussianProcessRegressor(
+            kernel=kernel, n_restarts_optimizer=9, random_state=42
+        )
         self.y_mean = None
         self.y_std = None
-
 
     def fit(self, df):
         dff = df.set_index(["Date"], drop=False)
@@ -102,20 +110,25 @@ class Forecaster:
         self.X_mean = X.mean()
         self.X_std = X.std()
         orig_date = X["Date"].copy()
-        self.X = (X-self.X_mean)/(self.X_std + 1e-9)
+        self.X = (X - self.X_mean) / (self.X_std + 1e-9)
         self.X["Date"] = orig_date
 
         self.y_mean = y.mean()
         self.y_std = y.std()
         self.y = (y - self.y_mean) / (self.y_std)
 
-
-    def forecast(self, steps: int, external_actions: List[ExternalAction]=[], maxlags=15, verbose=False):
+    def forecast(
+        self,
+        steps: int,
+        external_actions: List[ExternalAction] = [],
+        maxlags=15,
+        verbose=False,
+    ):
         stds = []
         preds = []
         start_time = int(self.X.iloc[-1]["Date"]) + 1
 
-        with warnings.catch_warnings(action="ignore"):
+        with warnings.catch_warnings():
             for _ in tqdm(range(steps), disable=not verbose):
                 model = VAR(self.X)
                 results = model.fit(maxlags=maxlags)
@@ -123,7 +136,7 @@ class Forecaster:
                 if not results.is_stable:
                     raise ValueError("Model is unstable. Check data or adjust lags.")
 
-                forecast_input = self.X.values[-results.k_ar:]
+                forecast_input = self.X.values[-results.k_ar :]
                 forecast = results.forecast(y=forecast_input, steps=1)
                 new_sample = {col: val for col, val in zip(self.X.columns, forecast[0])}
                 new_sample["Date"] = start_time
@@ -136,12 +149,17 @@ class Forecaster:
                             new_sample[e.var] = e.var
                         elif e.type == "update":
                             # De-normalize
-                            new_sample[e.var] = new_sample[e.var] * self.X_std[e.var] + self.X_mean[e.var]
+                            new_sample[e.var] = (
+                                new_sample[e.var] * self.X_std[e.var]
+                                + self.X_mean[e.var]
+                            )
                             new_sample[e.var] *= e.amount
                             # Ri-normalize
-                            new_sample[e.var] = (new_sample[e.var] - self.X_mean[e.var]) / self.X_std[e.var]
+                            new_sample[e.var] = (
+                                new_sample[e.var] - self.X_mean[e.var]
+                            ) / self.X_std[e.var]
 
-                self.X = pd.concat([ self.X, pd.DataFrame.from_dict([new_sample]) ])
+                self.X = pd.concat([self.X, pd.DataFrame.from_dict([new_sample])])
 
                 start_time += 1
 
@@ -151,17 +169,18 @@ class Forecaster:
 
             self.y = self.y * self.y_std + self.y_mean
 
-            self.gp.fit(self.X.iloc[:len(self.y)], self.y)
-            preds, stds = self.gp.predict(self.X.iloc[len(self.y):], return_std=True)
-            self.y = pd.concat([ self.y, pd.Series(preds) ])
+            self.gp.fit(self.X.iloc[: len(self.y)], self.y)
+            preds, stds = self.gp.predict(self.X.iloc[len(self.y) :], return_std=True)
+            self.y = pd.concat([self.y, pd.Series(preds)])
 
         return preds, stds
-    
 
     def explain_with_shap(self, samples, plot_type="bar"):
-    
+
         if self.gp is None or not hasattr(self.gp, "kernel_"):
-            raise ValueError("Il modello GP non è stato addestrato. Chiama 'fit' prima di utilizzare SHAP.")
+            raise ValueError(
+                "Il modello GP non è stato addestrato. Chiama 'fit' prima di utilizzare SHAP."
+            )
 
         # Seleziona un sottoinsieme di dati da spiegare
         X_sample = (self.X).sample(n=samples, random_state=42)
@@ -172,21 +191,27 @@ class Forecaster:
         # Calcola i valori SHAP
         shap_values = explainer.shap_values(X_sample.values)
 
-        if 'Date' in X_sample.columns:
-            date_idx = X_sample.columns.get_loc('Date')
+        if "Date" in X_sample.columns:
+            date_idx = X_sample.columns.get_loc("Date")
             X_sample = X_sample.drop(columns=["Date"])
             shap_values = np.delete(shap_values, date_idx, axis=1)
 
-        shap_dict = {'X' : [feature for feature in X_sample.columns], 'y' : [float(np.mean(np.abs(shap_values[:, idx]))) for idx in range(len(X_sample.columns))]}
-        
+        shap_dict = {
+            "X": [feature for feature in X_sample.columns],
+            "y": [
+                float(np.mean(np.abs(shap_values[:, idx])))
+                for idx in range(len(X_sample.columns))
+            ],
+        }
+
         # Visualizza i risultati
-        #if plot_type == "bar":
+        # if plot_type == "bar":
         #    shap.summary_plot(shap_values, X_sample, plot_type="bar")
-        #elif plot_type == "summary":
+        # elif plot_type == "summary":
         #    shap.summary_plot(shap_values, X_sample)
-        #elif plot_type == "force":
+        # elif plot_type == "force":
         #    shap.force_plot(explainer.expected_value, shap_values, self.X.iloc[:samples])
-        #else:
+        # else:
         #    raise ValueError(f"Tipo di plot SHAP '{plot_type}' non supportato.")
 
         return shap_dict
--- web/server.py
+++ web/server.py
@@ -1,11 +1,16 @@
 import sys
+
 sys.path.append("../src")
 
 from typing import Union
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-from data_loader import load_innovix_floresland, load_innovix_elbonie, load_bristor_zegoland
+from data_loader import (
+    load_innovix_floresland,
+    load_innovix_elbonie,
+    load_bristor_zegoland,
+)
 from Forecaster import Forecaster, ExternalAction
 import dateutil.parser
 import datetime
@@ -18,7 +23,6 @@ forecaster_orig = Forecaster()
 forecaster_orig.fit(df)
 
 curr_forecaster = None
-
 
 
 app = FastAPI()
@@ -34,42 +38,53 @@ class Request(BaseModel):
 
 def iso_to_timestep(iso_time, first_date):
     query_time = dateutil.parser.isoparse(iso_time)
-    return ((first_date.year - query_time.year) * 12 + first_date.month - query_time.month ) * -1
+    return (
+        (first_date.year - query_time.year) * 12 + first_date.month - query_time.month
+    ) * -1
 
 
-@app.post("/forecast/")
-async def forecast_req(requests: list[Request]):
+@app.post("/forecast/{num_steps}")
+async def forecast_req(num_steps: int, requests: list[Request]):
     global curr_forecaster
     curr_forecaster = deepcopy(forecaster_orig)
 
     preds, stds = curr_forecaster.forecast(
-        30,
-        external_actions = [
+        num_steps,
+        external_actions=[
             ExternalAction(
-                req.var, 
-                req.amount, 
-                iso_to_timestep(req.time, first_date), 
-                req.duration, 
-                req.actionType
-            ) for req in requests
+                req.var,
+                req.amount,
+                iso_to_timestep(req.time, first_date),
+                req.duration,
+                req.actionType,
+            )
+            for req in requests
         ],
-        maxlags = 15,
-        verbose = False,
+        maxlags=15,
+        verbose=False,
     )
 
-    return { 
-        "x": [*range(0, len(preds))], 
-        "y": preds.tolist(), 
-        "std": (stds*1.96).tolist(), 
-        "first_date": first_date + relativedelta(months=df.iloc[-1]["Date"]+1)
+    return {
+        "x": [*range(0, len(preds))],
+        "y": preds.tolist(),
+        "std": (stds * 1.96).tolist(),
+        "first_date": first_date + relativedelta(months=df.iloc[-1]["Date"] + 1),
     }
 
 
 @app.post("/shap/")
 async def shap_req():
-    return curr_forecaster.explain_with_shap(30)
+    result = curr_forecaster.explain_with_shap(20)
+    # Sort the result by the shapley values
+    features = result["X"]
+    values = result["y"]
 
-
+    # Sort the features by the shapley values
+    features = [x for _, x in sorted(zip(values, features), reverse=True)]
+    values = sorted(values, reverse=True)
+    result["X"] = features[:5]
+    result["y"] = values[:5]
+    return result
 
 
 app.mount("/", StaticFiles(directory="static"), name="static")
